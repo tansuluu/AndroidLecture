95d26833b3a9311233361675c2ccc87179880572
tansuluu25 <tansuluu.myrzaeva@iaau.edu.kg>
2018-10-11 18:58:51 +0600

    P0161_DynamicLayout lesson started

---
+++ p0161_dynamiclayout/src/main/java/ru/android/start/p0161_dynamiclayout/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.android.start.p0161_dynamiclayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LinearLay
+    }
+}
