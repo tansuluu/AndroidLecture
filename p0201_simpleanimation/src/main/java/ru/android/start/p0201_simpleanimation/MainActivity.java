c960e8c5d623bed53d82eead12766a9e00b69afe
tansuluu25 <tansuluu.myrzaeva@iaau.edu.kg>
2018-10-18 23:50:36 +0600

    Xml files for animation created

---
+++ p0201_simpleanimation/src/main/java/ru/android/start/p0201_simpleanimation/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.android.start.p0201_simpleanimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
