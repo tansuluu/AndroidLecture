a52ffba53bda794fb206af68dc009f732b03b91d
tansuluu25 <tansuluu.myrzaeva@iaau.edu.kg>
2018-10-23 23:28:18 +0600

    26 lesson finished

---
+++ p0271_getintentaction/src/main/java/ru/android/start/p0271_getintentaction/Info.java
@@ -0,0 +1,39 @@
+package ru.android.start.p0271_getintentaction;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        Intent intent = getIntent();
+        // читаем из него action
+        String action = intent.getAction();
+
+        String format = "", textInfo = "";
+
+        // в зависимости от action заполняем переменные
+        if (action.equals("ru.startandroid.intent.action.showtime")) {
+            format = "HH:mm:ss";
+            textInfo = "Time: ";
+        }
+        else if (action.equals("ru.startandroid.intent.action.showdate")) {
+            format = "dd.MM.yyyy";
+            textInfo = "Date: ";
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        String datetime = sdf.format(new Date(System.currentTimeMillis()));
+
+        TextView tvDate = (TextView) findViewById(R.id.tvInfo);
+        tvDate.setText(textInfo + datetime);
+    }
+}
---
+++ p0271_getintentaction/src/main/java/ru/android/start/p0271_getintentaction/MainActivity.java
@@ -0,0 +1,36 @@
+package ru.android.start.p0271_getintentaction;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnTime = (Button) findViewById(R.id.btnTime);
+        Button btnDate = (Button) findViewById(R.id.btnDate);
+
+        btnTime.setOnClickListener(this);
+        btnDate.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.btnTime:
+                intent = new Intent("ru.startandroid.intent.action.showtime");
+                startActivity(intent);
+                break;
+            case R.id.btnDate:
+                intent = new Intent("ru.startandroid.intent.action.showdate");
+                startActivity(intent);
+                break;
+        }
+    }
+}
