a2030d14bc8dea8a09afc0eb6499116da7838244
tansuluu25 <tansuluu.myrzaeva@iaau.edu.kg>
2018-10-30 21:59:53 +0600

    46 lesson FINISHED

---
+++ myapplication/src/main/java/ru/android/start/myapplication/P0481_SimpleAdapter.java
@@ -0,0 +1,13 @@
+package ru.android.start.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class P0481_SimpleAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p0481__simple_adapter);
+    }
+}
