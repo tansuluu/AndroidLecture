36faa7df856cdc654f9759686c3209055968e505
tansuluu25 <tansuluu.myrzaeva@iaau.edu.kg>
2018-10-31 23:32:10 +0600

    47 lesson FINISHED

---
+++ P0481_SimpleAdapter/src/main/java/ru/android/start/myapplication/P0481_SimpleAdapter.java
@@ -0,0 +1,52 @@
+package ru.android.start.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class P0481_SimpleAdapter extends AppCompatActivity {
+
+    final String ATTRIBUTE_NAME_TEXT = "text";
+    final String ATTRIBUTE_NAME_CHECKED = "checked";
+    final String ATTRIBUTE_NAME_IMAGE = "image";
+
+    ListView lvSimple;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p0481__simple_adapter);
+
+        String[] texts = { "sometext 1", "sometext 2", "sometext 3",
+                "sometext 4", "sometext 5" };
+        boolean[] checked = { true, false, false, true, false };
+        int img = R.drawable.ic_launcher_foreground;
+
+        ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>(
+                texts.length);
+        Map<String, Object> m;
+        for (int i = 0; i < texts.length; i++) {
+            m = new HashMap<String, Object>();
+            m.put(ATTRIBUTE_NAME_TEXT, texts[i]);
+            m.put(ATTRIBUTE_NAME_CHECKED, checked[i]);
+            m.put(ATTRIBUTE_NAME_IMAGE, img);
+            data.add(m);
+        }
+
+        String[] from = { ATTRIBUTE_NAME_TEXT, ATTRIBUTE_NAME_CHECKED,
+                ATTRIBUTE_NAME_IMAGE };
+        int[] to = { R.id.tvText, R.id.cbChecked, R.id.ivImg };
+
+        SimpleAdapter sAdapter = new SimpleAdapter(this, data, R.layout.item,
+                from, to);
+
+        // определяем список и присваиваем ему адаптер
+        lvSimple = (ListView) findViewById(R.id.lvSimple);
+        lvSimple.setAdapter(sAdapter);
+    }
+}
