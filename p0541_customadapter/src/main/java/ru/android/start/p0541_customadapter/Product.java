d234c16cb090af454be65d731aa79938dfb69e01
tansuluu25 <tansuluu.myrzaeva@iaau.edu.kg>
2018-11-18 21:28:09 +0600

    54 lesson finished. OwnAdapter

---
+++ p0541_customadapter/src/main/java/ru/android/start/p0541_customadapter/Product.java
@@ -0,0 +1,18 @@
+package ru.android.start.p0541_customadapter;
+
+public class Product {
+
+    String name;
+    int price;
+    int image;
+    boolean box;
+
+
+    Product(String _describe, int _price, int _image, boolean _box) {
+        name = _describe;
+        price = _price;
+        image = _image;
+        box = _box;
+    }
+
+}
