df31ca770e342e444c5e2fcb0fef9d82bb755e1b
tansuluu25 <tansuluu.myrzaeva@iaau.edu.kg>
2018-11-19 22:03:03 +0600

    My version TimePickerDialog and DatePickerDialog

--- p0591_datepickerdialog/src/main/java/ru/android/start/p0591_datepickerdialog/MainActivity.java
+++ p0591_datepickerdialog/src/main/java/ru/android/start/p0591_datepickerdialog/MainActivity.java
@@ -2,11 +2,13 @@ package ru.android.start.p0591_datepickerdialog;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,19 +17,30 @@ public class MainActivity extends AppCompatActivity {
     int myMonth = 02;
     int myDay = 03;
     TextView tvDate;
+    int DIALOG_TIME = 2;
+    int myHour = 14;
+    int myMinute = 35;
+    TextView tvTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvDate = (TextView) findViewById(R.id.tvDate);
+        tvTime = (TextView) findViewById(R.id.tvTime);
     }
     public void onclick(View view) {
         showDialog(DIALOG_DATE);
     }
 
-
+    public void onclickTime(View view) {
+        showDialog(DIALOG_TIME);
+    }
     protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_TIME) {
+            TimePickerDialog tpd = new TimePickerDialog(this, myCallBackTime, myHour, myMinute, true);
+            return tpd;
+        }
         if (id == DIALOG_DATE) {
             DatePickerDialog tpd = new DatePickerDialog(this, myCallBack, myYear, myMonth, myDay);
             return tpd;
@@ -43,6 +56,13 @@ public class MainActivity extends AppCompatActivity {
             myMonth = monthOfYear;
             myDay = dayOfMonth;
             tvDate.setText("Today is " + myDay + "/" + myMonth + "/" + myYear);
+        }
+    };
+    TimePickerDialog.OnTimeSetListener myCallBackTime = new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            myHour = hourOfDay;
+            myMinute = minute;
+            tvTime.setText("Time is " + myHour + " hours " + myMinute + " minutes");
         }
     };
 }
