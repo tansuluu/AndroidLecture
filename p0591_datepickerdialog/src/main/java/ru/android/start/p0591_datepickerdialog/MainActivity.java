e344f0a1c10c40eecffd5c32b9fb09bff6907f74
tansuluu25 <tansuluu.myrzaeva@iaau.edu.kg>
2018-11-19 21:59:09 +0600

    59 lesson finished.

--- p0591_datepickerdialog/src/main/java/ru/android/start/p0591_datepickerdialog/MainActivity.java
+++ p0591_datepickerdialog/src/main/java/ru/android/start/p0591_datepickerdialog/MainActivity.java
@@ -1,13 +1,48 @@
 package ru.android.start.p0591_datepickerdialog;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int DIALOG_DATE = 1;
+    int myYear = 2011;
+    int myMonth = 02;
+    int myDay = 03;
+    TextView tvDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvDate = (TextView) findViewById(R.id.tvDate);
     }
+    public void onclick(View view) {
+        showDialog(DIALOG_DATE);
+    }
+
+
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_DATE) {
+            DatePickerDialog tpd = new DatePickerDialog(this, myCallBack, myYear, myMonth, myDay);
+            return tpd;
+        }
+        return super.onCreateDialog(id);
+    }
+
+    DatePickerDialog.OnDateSetListener myCallBack = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            myYear = year;
+            myMonth = monthOfYear;
+            myDay = dayOfMonth;
+            tvDate.setText("Today is " + myDay + "/" + myMonth + "/" + myYear);
+        }
+    };
 }
