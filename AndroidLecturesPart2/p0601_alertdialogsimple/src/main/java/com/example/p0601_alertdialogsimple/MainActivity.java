723a47649605db66f4e351130843e970afaf6592
tanya <tansuluu.myrzaeva@iaau.edu.kg>
2018-11-23 01:51:11 +0600

    Continuous in AndroidLecturesPart2

---
+++ AndroidLecturesPart2/p0601_alertdialogsimple/src/main/java/com/example/p0601_alertdialogsimple/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.p0601_alertdialogsimple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
